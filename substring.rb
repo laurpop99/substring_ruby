4434cf107b7af78bf2f92ee3ff9923855c20ee64
Popescu Laurentiu <laurpop99@yahoo.com>
2020-09-13 17:00:46 +0300

    Modified substring.rb. Completed the Substring Ruby Project

--- substring.rb
+++ substring.rb
@@ -1,13 +1,21 @@
-word = gets
-puts word
-def substrings(word)
-  dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-  if dictionary.include?(word) 
-    puts "The word #{word} is in the dictionary".gsub("\n", "")
-  else 
-    puts "The word #{word} is not in the dictionary".gsub("\n", "")
+def substrings(word,dictionary)
+  hash = {}
+  word = word.split(" ")
+  for i in 0..word.length-1
+    for x in 0..dictionary.length-1
+      if word[i].downcase.include?(dictionary[x])
+        if hash.has_key?(dictionary[x])
+        hash[dictionary[x]] += 1
+        else
+          hash[dictionary[x]] = 1
+        end 
+      
+      end 
+    end
   end
+  p hash
+end 
 
-end
 
-  substrings(word)
+dictionary = ["below", "down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+substrings("Howdy partner, sit down! How's it going?",dictionary)
